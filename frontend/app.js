e6d3c610ec6b0572e5e02389a8ad6606426463c7
sinee99 <frog9032@naver.com>
2025-05-27 15:05:46 +0900

    first commit

---
+++ frontend/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AnalyzeScreen from './screens/AnalyzeScreen';
+import MatchScreen from './screens/MatchScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="분석" component={AnalyzeScreen} />
+        <Tab.Screen name="유실견 찾기" component={MatchScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
